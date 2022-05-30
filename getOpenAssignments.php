c1f67432ee837201ccdc2d0c2abcb2b4c24a784c
Juliet van Bezooijen <jay.vanbezooijen@student.avans.nl>
2022-05-30 11:44:55 +0200

    css table getAssignments

--- getOpenAssignments.php
+++ getOpenAssignments.php
@@ -9,7 +9,14 @@ $query = "SELECT * FROM request WHERE approved = 1";
 $result = $conn->query($query);
 
 if ($result->num_rows > 0) {
-    echo "<table><tr><th>Name</th><th>Summary</th><th>Date</th><th>Time</th><th>Place</th></tr>";
+    echo "<table>
+            <tr>
+                <th>Name</th>
+                <th>Summary</th>
+                <th>Date</th>
+                <th>Time</th>
+                <th>Place</th>
+            </tr>";
     // output data of each row
     while($row = $result->fetch_assoc()) {
       echo "<tr><td>".$row["requestName"]."</td><td>".$row["summary"]."</td><td>".$row["playDate"]."</td><td>".$row["playTime"]."</td><td>".$row["playGround"]."</td></tr>";
