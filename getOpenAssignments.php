2083c90c2c72d96b9b3d5e91b40ac686a7bc0516
Juliet van Bezooijen <jay.vanbezooijen@student.avans.nl>
2022-05-30 10:54:01 +0200

    Show overview open assignments

---
+++ getOpenAssignments.php
@@ -0,0 +1,23 @@
+<?php require 'database\dbconnection.php'; ?>
+<style>
+    <?php include 'css\table.css' ?>
+</style>
+
+
+<?php
+$query = "SELECT * FROM request WHERE approved = 1";
+$result = $conn->query($query);
+
+if ($result->num_rows > 0) {
+    echo "<table><tr><th>Name</th><th>Summary</th><th>Date</th><th>Time</th><th>Place</th></tr>";
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+      echo "<tr><td>".$row["requestName"]."</td><td>".$row["summary"]."</td><td>".$row["playDate"]."</td><td>".$row["playTime"]."</td><td>".$row["playGround"]."</td></tr>";
+    }
+    echo "</table>";
+  } else {
+    echo "0 results";
+  }
+
+  $conn->close();
+?>
