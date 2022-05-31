3b9d69b581984a277d5746d6b6dd27a8bf82b69c
Jens <25007405+JensdeVlaming@users.noreply.github.com>
2022-05-31 21:44:48 +0200

    Applied MVC and updated login page

--- app/views/user/login.php
+++ app/views/user/login.php
@@ -18,26 +18,26 @@ session_start();
     <link rel="stylesheet" type="text/css" href="src/css/styles.css">
 
 
-    <title>Lotus | Login</title>
+    <title><?php echo SITENAME ?> | Login</title>
 </head>
 
 <body>
     <div class="d-flex row g-0 vh-100">
         <div class="d-flex align-items-center justify-content-center ">
-            <form method="POST" action="/app">
+            <form method="POST">
                 <img src="/src/img/logo.svg" class="img-fluid mx-auto d-block mb-5" alt="Logo Lotus">
 
                 <span class="fw-bold">Login op jouw account.</span>
-                <input type="email" name="email" class="form-control form-control-lg mt-2 mb-4 py-2 px-3 login-input" placeholder="E-mailadres" />
+                <input type="email" name="email" class="form-control form-control-lg mt-2 mb-4 py-2 px-3 login-input" placeholder="E-mailadres" value="<?PHP if (isset($data["email"])) { echo $data["email"]; } ?>" />
 
                 <input type="password" name="password" class="form-control form-control-lg mb-4 py-2 px-3 login-input" placeholder="Wachtwoord" />
 
                 <button type="submit" name="req" value="login" class="btn btn-primary btn-lg fw-bold btn-login col-12 mb-2 login-submit">Inloggen</button>
 
-                <?php if (isset($_GET['err']) && $_GET['err'] == 1) { ?>
+                <?php if (isset($data["error"])) { ?>
 
                     <div class="alert alert-danger" id="alert-login" role="alert">
-                        <span class="text-center">De e-mailadres of wachtwoord niet geldig.</span>
+                        <span class="text-center">E-mailadres of wachtwoord niet geldig.</span>
                     </div>
 
                 <?php } ?>
