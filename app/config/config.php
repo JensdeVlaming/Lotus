8f999ebbf651483ffcb7a224103bf6437a96cf3a
Jens <25007405+JensdeVlaming@users.noreply.github.com>
2022-05-31 21:44:48 +0200

    Applied MVC and updated login page

---
+++ app/config/config.php
@@ -0,0 +1,17 @@
+<?php
+    // DATABASE
+    define("DB_HOST", "localhost");
+    define("DB_USER", "root");
+    define("DB_PASS", "");
+    define("DB_NAME", "lotus");
+
+
+    // SITENAME
+    define("SITENAME", "Lotus");
+
+    // APPROOT
+    define("APPROOT", dirname(dirname(__FILE__)));
+
+    // URLROOT
+    define("URLROOT", "localhost");
+
