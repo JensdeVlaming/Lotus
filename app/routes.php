1d6da43a6d56a30a11314958d443652ff4ce26af
Juliet van Bezooijen <jay.vanbezooijen@student.avans.nl>
2022-06-01 10:18:28 +0200

    Fixed routes error

--- app/routes.php
+++ app/routes.php
@@ -6,10 +6,7 @@ foreach (glob("../app/controllers/*.controller.php") as $filename) {
 
 // GET Requests
 Route::get("/login", [ViewController::class, "login"]);
-Route::get("/overview", function(){
-    MemberController::showAssignmentOverview;
-});
-
+Route::get("/overview", [MemberController::class, "showAssignmentOverview"]);
 
 // POST Requests
 Route::post("/login", [[UserController::class, "login"], ]);
