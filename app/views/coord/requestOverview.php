1537bc603690ecb0b60fb9473ec3eb477fb57864
Juliet van Bezooijen <jay.vanbezooijen@student.avans.nl>
2022-06-02 10:11:43 +0200

    Finished card overview of member and coordinator

--- app/views/coord/requestOverview.php
+++ app/views/coord/requestOverview.php
@@ -4,78 +4,78 @@
 <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="/src/css/styles.css">
+    <!-- FontAwesome -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
 
     <title><?php echo SITENAME ?> | Opdracht aanvragen</title>
 </head>
 
 <body>
+    <div class="container">
+        <?php
+        foreach ($data as $item) {
 
-    <section class="container text-center mt-1">
-        <form method="POST">
-            <input type="submit" class="btn btn-outline-primary btn-lg" name="requests" value="Opdracht aanvragen">
-            <input type="submit" class="btn btn-outline-dark btn-lg" name="assignMembers" value="Leden toewijzen">
-        </form>
-        <hr class="dropdown-divider">
-    </section>
+        ?>
 
-    <section>
-
-
-        <?php foreach ($data as $item) { ?>
-            <div class="card shadow-lg m-3 mx-auto" style="width: 32rem;">
-                <div class="row m-2 ">
-                    <div class="col-md-3">
-                        <div class="card-det">
-                            <h5><?php echo "" . $item["requestName"] . "" ?></h5>
-                            <h6><?php echo "" . $item["playDate"] . "" ?></h6>
-                            <h6><?php echo "" . $item["playGround"] . "" ?></h6>
+            <div class="card shadow-lg my-2 mx-1 mx-auto">
+                <div class="card-body">
+                    <div class="row gx-5">
+                        <div class="col col-12 col-md-auto">
+                            <h5 class="card-title"><?php echo $item["summary"] ?></h5>
+                            <h6 class="card-subtitle mb-2 text-muted"><?php echo $item["requestName"] ?></h6>
+                            <h6 class="card-subtitle mb-2 text-muted"><?php echo $item["playDate"] ?> - <?php echo $item["playTime"] ?></h6>
                         </div>
-                    </div>
-
-                    <div class="col-md-9">
-                        <div class="card-map">
-                            <div id="map-container-google-1" class="z-depth-1-half map-container" style="height: 160px">
-                                <iframe src="https://maps.google.com/maps?q=<?php echo "" . $item["playGround"] . "" ?>&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" style="border:0" allowfullscreen></iframe>
+                        <div class="col">
+                            <div class="embed-responsive text-center col-12">
+                                <iframe class="col-12" src="https://maps.google.com/maps?q=<?php echo "" . $item["playCity"] . "+" . $item["playStreet"] . "+" . $item["playPremise"] . "+" . $item["playPCode"] . "" ?>&t=&z=13&ie=UTF8&iwloc=&output=embed"></iframe>
+                            </div>
+                            <div class="row g-1">
+                                <span class="col col-6" href="/opdracht/<?php echo $item["id"] ?>/afwijzen"><button type="button" class="btn btn-danger m-0 col-12" data-bs-toggle="modal" data-bs-target="#confirmationModal">Afwijzen</button></span>
+                                <span class="col col-6" href="/opdracht/<?php echo $item["id"] ?>/aanmelden"><button type="button" class="btn btn-success m-0 col-12" data-bs-toggle="modal" data-bs-target="#confirmationModal">Accepteren</button></span>
                             </div>
                         </div>
-                        <div class="card-registration text-center">
-                            <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#confirmationModal">Accepteren</button>
-                            <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#confirmationModal">Weigeren</button>
+                    </div>
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item"><strong>Speellocatie: </strong> <?php echo $item["playStreet"] . " " . $item["playPremise"] . ", " . $item["playCity"] ?></li>
+                    <li class="list-group-item"><strong>Grimeerlocatie: </strong> <?php echo $item["grimeStreet"] . " " . $item["grimePremise"] . ", " . $item["grimeCity"] ?></li>
+                </ul>
+                <div class="card-footer">
+                    <small class="text-muted">3 dagen geleden</small>
+                </div>
+            </div>
+
+            <!-- Modal -->
+            <div class="modal fade" id="confirmationModal" tabindex="-1" aria-labelledby="confirmationModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="confirmationModalLabel">Weet u het zeker?</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            Deze actie kan niet ongedaan worden.
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="cancelButton btn" data-bs-dismiss="modal" >Annuleren</button>
+                            <button type="button" class="acceptButton btn">Ga verder</button>
                         </div>
                     </div>
                 </div>
             </div>
-        <?php } ?>
-        </div>
-        </div>
-    </section>
-
-    <!-- Modal -->
-    <div class="modal fade" id="confirmationModal" tabindex="-1" aria-labelledby="confirmationModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="confirmationModalLabel">Weet u het zeker?</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    Deze actie kan niet ongedaan worden.
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="cancelButton btn" data-bs-dismiss="modal">Annuleren</button>
-                    <button type="button" class="acceptButton btn">Ga verder</button>
-                </div>
-            </div>
-        </div>
+        <?php
+        }
+        ?>
     </div>
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
 </body>
 
 </html>
--- app/views/member/overview.php
+++ app/views/member/overview.php
@@ -4,54 +4,66 @@
 <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- FontAwesome -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
 
-    <link rel="stylesheet" type="text/css" href="src/css/styles.css">
-
-
-    <title><?php echo SITENAME ?> | Opdracht aanvragen</title>
+    <title><?php echo SITENAME ?> | Opdrachten</title>
 </head>
 
 <body>
-    <section>
-        <section class="container text-center mt-1">
-            <form method="POST">
-                <input type="submit" class="btn btn-outline-primary btn-lg" name="requests" value="Jouw opdrachten">
-                <input type="submit" class="btn btn-outline-dark btn-lg" name="newReq" value="Nieuwe opdrachten">
-            </form>
-            <hr class="dropdown-divider">
+    <div class="container">
+        <?php
+        foreach ($data as $item) {
 
-        </section>
+            if ($item["approved"] == 0) {
+                $approved = '<span class="text-muted">Afgewezen:</span> <i class="fa fa-times text-danger" aria-hidden="true"></i>';
+            } else if ($item["approved"] == 1) {
+                $approved = '<span class="text-muted">Goedgekeurd:</span> <i class="fa fa-check text-success" aria-hidden="true"></i>';
+            } else if ($item["approved"] == 2) {
+                $approved = '<span class="text-muted">Wachtende:</span> <i class="fa fa-clock-o text-warning" aria-hidden="true"></i>';
+            }
+        ?>
 
-        <?php foreach ($data as $item) { ?>
-            <div class="card shadow-lg m-3 mx-auto" style="width: 32rem;">
-                <div class="row m-2 ">
-                    <div class="col-md-3">
-                        <div class="card-det">
-                            <h5><?php echo "" . $item["requestName"] . "" ?></h3>
-                                <h6><?php echo "" . $item["playDate"] . "" ?></h4>
-                                    <h6><?php echo "" . $item["playGround"] . "" ?></h4>
+            <div class="card shadow-lg my-2 mx-1 mx-auto">
+                <div class="card-body">
+                    <div class="row gx-5">
+                        <div class="col col-12 col-md-auto">
+                            <h5 class="card-title"><?php echo $item["summary"] ?></h5>
+                            <h6 class="card-subtitle mb-2 text-muted"><?php echo $item["requestName"] ?></h6>
+                            <h6 class="card-subtitle mb-2 text-muted"><?php echo $item["playDate"] ?> - <?php echo $item["playTime"] ?></h6>
+                            <h6 class="card-subtitle mb-2"><?php echo $approved ?></h6>
                         </div>
-                    </div>
-
-                    <div class="col-md-9">
-                        <div class="card-map">
-                            <div id="map-container-google-1" class="z-depth-1-half map-container" style="height: 160px">
-                                <iframe src="https://maps.google.com/maps?q=<?php echo "" . $item["playGround"] . "" ?>&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" style="border:0" allowfullscreen></iframe>
+                        <div class="col">
+                            <div class="embed-responsive text-center col-12">
+                                <iframe class="col-12" src="https://maps.google.com/maps?q=<?php echo "" . $item["playCity"] . "+" . $item["playStreet"] . "+" . $item["playPremise"] . "+" . $item["playPCode"] . "" ?>&t=&z=13&ie=UTF8&iwloc=&output=embed"></iframe>
                             </div>
-                        </div>
-                        <div class="card-registration text-center">
-                            <button class="btn btn-success">Inschrijven </button>
+                            <div class="row">
+                                <span class="col-12" href="/opdracht/<?php echo $item["id"] ?>/aanmelden"><button type="button" class="btn btn-success m-0 col-12">Aanmelden</button></span>
+                            </div>
                         </div>
                     </div>
                 </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item"><strong>Speellocatie: </strong> <?php echo $item["playStreet"] . " " . $item["playPremise"] . ", " . $item["playCity"] ?></li>
+                    <li class="list-group-item"><strong>Grimeerlocatie: </strong> <?php echo $item["grimeStreet"] . " " . $item["grimePremise"] . ", " . $item["grimeCity"] ?></li>
+                </ul>
+                <div class="card-footer">
+                    <small class="text-muted">3 dagen geleden</small>
+                </div>
             </div>
-
-        <?php } ?>
-        </div>
-        </div>
-    </section>
+        <?php
+        }
+        ?>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
 </body>
+
+</html>
