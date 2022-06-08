710ecbd108be4a683045c7a885db06c40b5b0465
Juliet van Bezooijen <jay.vanbezooijen@student.avans.nl>
2022-06-08 10:22:37 +0200

    changed sql script

--- lotus.sql
+++ lotus.sql
@@ -224,14 +224,14 @@ INSERT INTO request (requestId, description, comments, date, time, casualties, p
 INSERT INTO request (requestId, description, comments, date, time, casualties, playGroundId, grimeLocationId, companyId, contactId, billingAddressId) VALUES (3, "Hersenschudding", "Hoofdpijn", "14-06-2022", "12:00", 10, 3, 3, 2, 2, 3);
 
 -- solicit
--- 0 = denied / 1 = appointed / 2 = pending
+-- 0 = ingeschreven / 1 = toegewezen / 2 = niet toegewezen
 INSERT INTO solicit VALUES
 ('kasper@lotus.nl','1',2),
 ('juliet@lotus.nl','2',0),
 ('daniel@lotus.nl','1',1),
 ('jens@lotus.nl','1',0),
 ('membera@lotus.nl','3',1),
-('memberb@lotus.nl','5',2);
+('memberb@lotus.nl','3',2);
 
 
 
