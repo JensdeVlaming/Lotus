1aaa0cfd03a29b09b3daccdd261d4d30041cb88e
Jens <25007405+JensdeVlaming@users.noreply.github.com>
2022-06-08 19:51:38 +0200

    Changed config to support online database

--- app/config/config.php
+++ app/config/config.php
@@ -1,9 +1,9 @@
 <?php
     // DATABASE
-    define("DB_HOST", "localhost");
-    define("DB_USER", "root");
-    define("DB_PASS", "");
-    define("DB_NAME", "lotus");
+    define("DB_HOST", $_ENV["DB_HOST"] ?? "localhost");
+    define("DB_USER", $_ENV["DB_USER"] ?? "root");
+    define("DB_PASS", $_ENV["DB_PASS"] ?? "");
+    define("DB_NAME", $_ENV["DB_NAME"] ?? "lotus");
 
 
     // SITENAME
