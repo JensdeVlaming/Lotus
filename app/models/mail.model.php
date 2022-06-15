d4ee486af3c000a18cc2b8ac7784ade52dd4831f
Zuijd <dca.vanzuijdam@student.avans.nl>
2022-06-15 10:34:56 +0200

    changed mail controller to model

--- app/models/mail.model.php
+++ app/models/mail.model.php
@@ -1,5 +1,5 @@
 <?php
- 
+
 // Import PHPMailer classes into the global namespace 
 require '../vendor/autoload.php';
 // Import PHPMailer classes into the global namespace 
@@ -7,17 +7,14 @@ use PHPMailer\PHPMailer\PHPMailer;
 
 
 
-class MailController extends Controller {
+class MailModel
+{
 
-    public function __construct()
+    public function mail()
     {
-        parent::__construct();
-    }
 
-    public function mail() {
- 
         $mail = new PHPMailer;
- 
+
         $mail->isSMTP();                      // Set mailer to use SMTP 
         $mail->Host = 'smtp.gmail.com';       // Specify main and backup SMTP servers 
         $mail->SMTPAuth = true;               // Enable SMTP authentication 
@@ -25,32 +22,32 @@ class MailController extends Controller {
         $mail->Password = 'mqepvldycmkxtctm';  // SMTP password 
         $mail->SMTPSecure = 'tls';            // Enable TLS encryption, `ssl` also accepted 
         $mail->Port = 587;                    // TCP port to connect to 
-         
+
         // Sender info 
         $mail->setFrom('lotus.herewego@gmail.com', 'Lotus');
-         
+
         // Add a recipient 
-        $mail->addAddress('dvzdanieldvz@gmail.com'); 
-         
+        $mail->addAddress('dvzdanieldvz@gmail.com');
+
         //$mail->addCC('cc@example.com'); 
         //$mail->addBCC('bcc@example.com'); 
-         
+
         // Set email format to HTML 
-        $mail->isHTML(true); 
-         
+        $mail->isHTML(true);
+
         // Mail subject 
-        $mail->Subject = 'Email from Localhost by CodexWorld'; 
-         
+        $mail->Subject = 'Email from Localhost by CodexWorld';
+
         // Mail body content 
-        $bodyContent = '<h1>How to Send Email from Localhost using PHP by CodexWorld</h1>'; 
-        $bodyContent .= '<p>This HTML email is sent from the localhost server using PHP by <b>CodexWorld</b></p>'; 
-        $mail->Body    = $bodyContent; 
-         
+        $bodyContent = '<h1>How to Send Email from Localhost using PHP by CodexWorld</h1>';
+        $bodyContent .= '<p>This HTML email is sent from the localhost server using PHP by <b>CodexWorld</b></p>';
+        $mail->Body    = $bodyContent;
+
         // Send email 
-        if(!$mail->send()) { 
-            echo 'Message could not be sent. Mailer Error: '.$mail->ErrorInfo; 
-        } else { 
-            echo 'Message has been sent.'; 
-        } 
+        if (!$mail->send()) {
+            echo 'Message could not be sent. Mailer Error: ' . $mail->ErrorInfo;
+        } else {
+            echo 'Message has been sent.';
+        }
     }
 }
